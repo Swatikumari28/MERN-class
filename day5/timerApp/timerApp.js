66dfdeda42ada665610fcf691ffe4fa9636b7c6f
Swati Kumari <swatikrionly@gmail.com>
2025-06-29 23:43:27 +0530

    Stopwatch (callback function)

--- day5/timerApp/timerApp.js
+++ day5/timerApp/timerApp.js
@@ -1,37 +1,38 @@
 const timerTextElem = document.getElementById("timer-text");
-let timeInSec = 0;
-let intervalId = setInterval(timerFunc, 1000);
 
-function timerFunc() {
+let timeInSec = 0;
+const timerFunc = () => {
   timeInSec++;
 
   const sec = timeInSec % 60;
-  let min = Math.floor(timeInSec / 60) % 60;
+  const min = Math.floor(timeInSec / 60) % 60;
   const hrs = Math.floor(timeInSec / 3600);
 
   timerTextElem.innerText = `${hrs.toString().padStart(2, "0")}:${min
     .toString()
     .padStart(2, "0")}:${sec.toString().padStart(2, "0")}`;
-}
+};
 
-function handleReset() {
+let intervalId = setInterval(timerFunc, 1000);
+
+const handleReset = () => {
   timeInSec = 0;
   clearInterval(intervalId);
   timerTextElem.innerText = "00:00:00";
   document.getElementById("root").innerHTML = "";
-}
+};
 
-function handlePause() {
+const handlePause = () => {
   clearInterval(intervalId);
-}
+};
 
-function handleResume() {
+const handleResume = () => {
   clearInterval(intervalId);
   intervalId = setInterval(timerFunc, 1000);
-}
+};
 
-function handleLap() {
+const handleLap = () => {
   const para = document.createElement("p");
   para.innerText = timerTextElem.innerText;
   document.getElementById("root").appendChild(para);
-}
+};
